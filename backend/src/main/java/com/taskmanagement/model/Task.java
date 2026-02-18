e3ad7da0ac0103e4f9346b7411bf7f4f5b13064a
khatrivishal001-del <khatrivishal001@gmail.com>
2026-02-18 13:52:11 +0530

    Add getId() and setId() methods to Task model

--- backend/src/main/java/com/taskmanagement/model/Task.java
+++ backend/src/main/java/com/taskmanagement/model/Task.java
@@ -66,4 +66,12 @@ public class Task {
     public void setCompleted(boolean completed) {
         this.completed = completed;
     }
+
+        public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
